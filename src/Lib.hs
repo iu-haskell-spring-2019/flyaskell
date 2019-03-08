37148ad0bcb9e37d46d8a3e585ca80d67a4a5474
Nikolay Gaivoronskiy <cezarnik2@yandex.ru>
2019-03-08 23:52:55 +0300

    add density function and misc shit

--- src/Lib.hs
+++ src/Lib.hs
@@ -54,4 +54,18 @@ hessWPoly r h
   | r <= h = 315 / 64 / pi / h**9 * 6 * (h**2 - r**2) * (4 * r**2 - (h**2 - r**2))
   | otherwise = 0
 
+dist :: Coord -> Coord -> Double
+dist (x1,y1) (x2,y2) = ((x1 - x2)**2 + (y1 - y2)**2)**0.5
+
+type Water = [Particle]
+
+h :: Double
+h = 5
+
+calcDensity :: Water -> (Double -> Double -> Double) -> Coord -> Double
+calcDensity [] func coord = 0
+calcDensity (p:ps) func coord = (calcDensity ps func coord) +
+  ((mass p) * (func (dist (position p) coord) h))
+
+
 -- drawParticle :: Particle -> ???
