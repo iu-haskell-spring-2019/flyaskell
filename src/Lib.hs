243d0464965d027cd5ee82a149a44a3a27207e65
Nikita Vilunov <nikitaoryol@gmail.com>
2019-03-08 22:17:26 +0300

    kekekekke

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,31 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Lib
     ( run
     ) where
 
+import SDL
+import Linear (V4(..))
+import Control.Monad (unless)
+
 run :: IO ()
-run = putStrLn "PRIVET MIRRRRRR"
+run = do
+  initializeAll
+  window <- createWindow "My SDL Application" defaultWindow
+  renderer <- createRenderer window (-1) defaultRenderer
+  appLoop renderer
+
+appLoop :: Renderer -> IO ()
+appLoop renderer = do
+  events <- pollEvents
+  let eventIsQPress event =
+        case eventPayload event of
+          KeyboardEvent keyboardEvent ->
+            keyboardEventKeyMotion keyboardEvent == Pressed &&
+            keysymKeycode (keyboardEventKeysym keyboardEvent) == KeycodeQ
+          _ -> False
+      qPressed = any eventIsQPress events
+  rendererDrawColor renderer $= V4 0 0 255 255
+  clear renderer
+  present renderer
+  unless qPressed (appLoop renderer)
