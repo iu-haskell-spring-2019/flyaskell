a0a032adb415e0954f82973cfb8cf0c17f29430e
Nikolay Gaivoronskiy <cezarnik2@yandex.ru>
2019-03-08 23:15:07 +0300

    wrote data types and kernels

--- src/Lib.hs
+++ src/Lib.hs
@@ -29,3 +29,31 @@ appLoop renderer = do
   clear renderer
   present renderer
   unless qPressed (appLoop renderer)
+
+data Color = Green | Red | Blue
+type Coord = (Double, Double)
+data Particle = Particle { color :: Color
+                         , position :: Coord
+                         , velocity :: Coord
+                         , mass :: Double
+}
+
+pi :: Double
+pi = 3.141592653589793
+
+wPoly :: Double -> Double -> Double
+wPoly r h
+  | r <= h = 315/64/pi/h**9*(h**2-r**2)**3
+  | otherwise = 0
+
+gradWPoly :: Double -> Double -> Double
+gradWPoly r h
+  | r <= h = -315/64/pi/h**9*6*r*(h**2-r**2)**2
+  | otherwise = 0
+
+hessWPoly :: Double -> Double -> Double
+hessWPoly r h
+  | r <= h = 315/64/pi/h**9*6*(h**2-r**2)*(4*r**2-(h**2-r**2))
+  | otherwise = 0
+  
+-- drawParticle :: Particle -> ???
