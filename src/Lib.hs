0185b735c19b1cdf09ee4b358f3c48578e8aa41b
sultazat <sultazat@gmail.com>
2019-03-08 23:19:29 +0300

    bugs fixed

--- src/Lib.hs
+++ src/Lib.hs
@@ -38,22 +38,20 @@ data Particle = Particle { color :: Color
                          , mass :: Double
 }
 
-pi :: Double
-pi = 3.141592653589793
 
 wPoly :: Double -> Double -> Double
 wPoly r h
-  | r <= h = 315/64/pi/h**9*(h**2-r**2)**3
+  | r <= h = 315 / 64 / pi / h**9 * (h**2 - r**2)**3
   | otherwise = 0
 
 gradWPoly :: Double -> Double -> Double
 gradWPoly r h
-  | r <= h = -315/64/pi/h**9*6*r*(h**2-r**2)**2
+  | r <= h = -315 / 64 / pi / h**9 * 6 * r * (h**2 - r**2) ** 2
   | otherwise = 0
 
 hessWPoly :: Double -> Double -> Double
 hessWPoly r h
-  | r <= h = 315/64/pi/h**9*6*(h**2-r**2)*(4*r**2-(h**2-r**2))
+  | r <= h = 315 / 64 / pi / h**9 * 6 * (h**2 - r**2) * (4 * r**2 - (h**2 - r**2))
   | otherwise = 0
-  
+
 -- drawParticle :: Particle -> ???
